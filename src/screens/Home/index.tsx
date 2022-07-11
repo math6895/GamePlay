0742c9848c6b4f6d5267ce0a13dcaa98f7c03a21
math6895 <mateusoliveiraportugal@yahoo.com.br>
2022-07-11 13:18:06 -0300

    Apresentação dos Appointments na Home (feito)

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,95 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState , useCallback } from 'react';
 import { View, FlatList, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation , useFocusEffect } from '@react-navigation/native';
+import { COLLECTION_APPOINTMENTS } from '../../configs/database';
 
 import { Background } from '../../components/Background';
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { CategorySelect } from '../../components/CategorySelect';
 import { Profile } from '../../components/Profile';
 import { ListHeader } from '../../components/ListHeader';
-import { Appointment } from '../../components/Appointment';
+import { Appointment , AppointmentProps } from '../../components/Appointment';
 import { ListDivider } from '../../components/ListDivider';
+import { Load } from '../../components/Load';
 
 import { styles } from './styles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 
 export function Home() {
     const [category, setCategory] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [appointments, setAppointments] = useState<AppointmentProps[]>([]);
+
     const navigation = useNavigation();
-    const appointments = [
-        {
-            id: '1',
-            guild: {
-                id: '1',
-                name: 'Lendários',
-                icon: null,
-                owner: true
-            },
-            category: '1',
-            date: '24/07 às 19:20h',
-            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
-        },
-        {
-            id: '2',
-            guild: {
-                id: '2',
-                name: 'BR Team',
-                icon: null,
-                owner: false
-            },
-            category: '3',
-            date: '17/07 às 14:30h',
-            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
-        },
-        {
-            id: '3',
-            guild: {
-                id: '3',
-                name: 'Cloud9',
-                icon: null,
-                owner: false
-            },
-            category: '2',
-            date: '12/07 às 21:15h',
-            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
-        },
-        {
-            id: '4',
-            guild: {
-                id: '4',
-                name: 'INTZ',
-                icon: null,
-                owner: false
-            },
-            category: '4',
-            date: '10/07 às 21:00h',
-            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
-        },
-        {
-            id: '5',
-            guild: {
-                id: '5',
-                name: 'Team Liquid',
-                icon: null,
-                owner: true
-            },
-            category: '4',
-            date: '08/07 às 20:45h',
-            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
-        },
-        {
-            id: '6',
-            guild: {
-                id: '6',
-                name: 'FaZe Clan',
-                icon: null,
-                owner: false,
-            },
-            category: '1',
-            date: '010/07 às 15:30h',
-            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
-        }
-    ]
 
     function handleCategorySelect(categoryId: string) {
         categoryId === category ? setCategory('') : setCategory(categoryId);
@@ -102,6 +35,23 @@ export function Home() {
         navigation.navigate('AppointmentCreate');
     }
 
+    async function loadAppointments(){
+        const response = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS);
+        const storage: AppointmentProps[] = response ? JSON.parse(response) : [];
+
+        if(category){
+            setAppointments(storage.filter(item => item.category ===category));        
+        }else{
+            setAppointments(storage)
+        }
+
+        setLoading(false);
+    }
+
+    useFocusEffect(useCallback(() => {
+        loadAppointments();
+    },[category]));
+
     return (
         <Background>
             <View style={styles.header}>
@@ -112,24 +62,60 @@ export function Home() {
                 categorySelected={category}
                 setCategory={handleCategorySelect}
             />
-            <ListHeader
-                title="Partidas agendadas"
-                subtitle="Total 6"
-            />
-            <FlatList
-                data={appointments}
-                keyExtractor={item => item.id}
-                renderItem={({ item }) => (
-                    <Appointment 
-                        data={item} 
-                        onPress={handleAppointmentDetails}
+
+            
+                    <ListHeader
+                        title="Partidas agendadas"
+                        subtitle="Total 6"
+                    />               
+            {
+                loading ? <Load /> :
+                <>    
+                    <FlatList
+                        data={appointments}
+                        keyExtractor={item => item.id}
+                        renderItem={({ item }) => (
+                            <Appointment 
+                                data={item} 
+                                onPress={handleAppointmentDetails}
+                            />
+                        )}
+                        ItemSeparatorComponent={() => <ListDivider />}
+                        contentContainerStyle={{paddingBottom: 40}}
+                        style={styles.matches}
+                        showsVerticalScrollIndicator={false}
                     />
-                )}
-                ItemSeparatorComponent={() => <ListDivider />}
-                contentContainerStyle={{paddingBottom: 40}}
-                style={styles.matches}
-                showsVerticalScrollIndicator={false}
-                />
+                </>
+            }    
         </Background>
     );
 }
+
+/* OLD APPOINTMENTS
+const appointments = [
+    {
+        id: '1',
+        guild: {
+            id: '1',
+            name: 'Lendários',
+            icon: null,
+            owner: true
+        },
+        category: '1',
+        date: '24/07 às 19:20h',
+        description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+    },
+    {
+        id: '2',
+        guild: {
+            id: '2',
+            name: 'BR Team',
+            icon: null,
+            owner: false
+        },
+        category: '3',
+        date: '17/07 às 14:30h',
+        description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+    },
+]
+*/
