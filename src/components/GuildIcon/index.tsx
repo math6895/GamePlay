011b9f5ca449af0462440e0ce049859ec722902d
math6895 <mateusoliveiraportugal@yahoo.com.br>
2022-07-11 12:20:50 -0300

    Configuração dos GuildIcons obtidos do servidor do Discord

--- src/components/GuildIcon/index.tsx
+++ src/components/GuildIcon/index.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image , View } from 'react-native';
 
 import { styles } from './styles';
+import DiscordSvg  from '../../assets/discord.svg';
 
-export function GuildIcon(){
-    const uri = 'https://static.wikia.nocookie.net/40b1a6b4-887a-4b88-931c-4c8f0c404145';
+const {CDN_IMAGE} =process.env;
+
+type Props = {
+    guildId: string;
+    iconId: string | null;
+}
+
+export function GuildIcon({guildId, iconId}: Props){
+    const uri = `${CDN_IMAGE}/icons/${guildId}/${iconId}.png`;
+
+    //'https://static.wikia.nocookie.net/40b1a6b4-887a-4b88-931c-4c8f0c404145';
+
     return(
-        <Image 
-            source={{ uri }}
-            style={styles.image}
-            resizeMode="cover"
-        />   
+        <View style={styles.container}>
+            {
+                iconId ? 
+                <Image 
+                    source={{ uri }}
+                    style={styles.image}
+                    resizeMode="cover"
+                /> 
+                :
+                <DiscordSvg 
+                    width={40}
+                    height={40}
+                />               
+            }
+        </View>
     );
 }
